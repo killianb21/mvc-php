49f2ad189833399742c260a203e0c5bbbdcee59e
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-10-11 17:00:35 +0200

    Isolez l'affichage du traitement PHP

---
+++ mvc-php/blog/index.php
@@ -0,0 +1,26 @@
+<?php
+      // Connexion à la base de données
+      try
+      {
+         $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+      }
+      catch(Exception $e){
+            die( 'Erreur : '.$e->getMessage()   );
+      }
+
+      // On récupère les 5 derniers billets
+      $statement = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+      while ($row = $statement->fetch()){
+         $post = [
+            'title' => $row['titre'],
+            'content' => $row['contenu'],
+            'frenchCreationDate' => $row['date_creation_fr'],
+         ];
+
+         $posts[] = $post;
+
+      }
+
+         require('template/homepage.php');
+?>
