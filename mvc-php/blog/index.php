2b5ede83afb2d55a9cb3c8c3e4857739a64a8800
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-10-11 17:07:12 +0200

    Isolez l'accès aux données

--- mvc-php/blog/index.php
+++ mvc-php/blog/index.php
@@ -1,26 +1,5 @@
 <?php
-      // Connexion à la base de données
-      try
-      {
-         $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-      }
-      catch(Exception $e){
-            die( 'Erreur : '.$e->getMessage()   );
-      }
-
-      // On récupère les 5 derniers billets
-      $statement = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-      while ($row = $statement->fetch()){
-         $post = [
-            'title' => $row['titre'],
-            'content' => $row['contenu'],
-            'frenchCreationDate' => $row['date_creation_fr'],
-         ];
-
-         $posts[] = $post;
-
-      }
-
-         require('template/homepage.php');
+require('src/model.php');
+$posts = getPosts();
+require('template/homepage.php');
 ?>
