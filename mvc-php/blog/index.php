83d1be966fc1bd8bb6d0e3c1a7bf44b815a2b6d9
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-10-18 15:24:49 +0200

    Gérez les erreurs

--- mvc-php/blog/index.php
+++ mvc-php/blog/index.php
@@ -3,26 +3,28 @@
 require_once('src/controllers/add_comment.php');
 require_once('src/controllers/homepage.php');
 require_once('src/controllers/post.php');
+try {
 if (isset($_GET['action']) && $_GET['action'] !== '') {
 if ($_GET['action'] === 'post') {
 if (isset($_GET['id']) && $_GET['id'] > 0) {
 $identifier = $_GET['id'];
 post($identifier);
 } else {
-echo 'Erreur : aucun identifiant de billet envoyé';
-die;
+throw new Exception('Aucun identifiant de billet envoyé');
 }
 } elseif ($_GET['action'] === 'addComment') {
 if (isset($_GET['id']) && $_GET['id'] > 0) {
 $identifier = $_GET['id'];
 addComment($identifier, $_POST);
 } else {
-echo 'Erreur : aucun identifiant de billet envoyé';
-die;
+throw new Exception('Aucun identifiant de billet envoyé');
 }
 } else {
-echo "Erreur 404 : la page que vous recherchez n'existe pas.";
+throw new Exception("La page que vous recherchez n'existe pas.");
 }
 } else {
 homepage();
+}
+} catch (Exception $e) { // S'il y a eu une erreur, alors...
+echo 'Erreur : '.$e->getMessage();
 }
