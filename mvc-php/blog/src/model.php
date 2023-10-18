c8414b0797f6100d27e32459d837f94e470a1d17
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-10-18 15:07:19 +0200

    Ajoutez des commentaires

--- mvc-php/blog/src/model.php
+++ mvc-php/blog/src/model.php
@@ -37,28 +37,6 @@ function getPost($identifier) {
     return $post;
 }
 
-function getComments($identifier)
-{
-    $database = dbConnect();
-    $statement = $database->prepare(
-        "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
-    );
-    $statement->execute([$identifier]);
-
-    $comments = [];
-    while (($row = $statement->fetch())) {
-        $comment = [
-            'author' => $row['author'],
-            'french_creation_date' => $row['french_creation_date'],
-            'comment' => $row['comment'],
-        ];
-
-        $comments[] = $comment;
-    }
-
-    return $comments;
-}
-
 function dbConnect()
 {
     try {
@@ -68,4 +46,5 @@ function dbConnect()
     } catch(Exception $e) {
         die('Erreur : '.$e->getMessage());
     }
+
 }
